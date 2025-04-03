b9ddaf4ffe81d99ea995b6ddc8149b1bfb502b00
mikaeloduh <mikaeloduh@users.noreply.github.com>
2025-04-04 01:26:42 +0800

    docs: Improve Encoder and Decoder documentation with detailed type descriptions

--- pkg/expressgo/decoder.go
+++ pkg/expressgo/decoder.go
@@ -2,5 +2,7 @@ package expressgo
 
 import "io"
 
-// Decoder is a function interface that decodes a reader into an object
+// Decoder is a function type that defines an interface for decoding data.
+// It represents a function that takes an io.Reader and a destination object,
+// then decodes the data from the reader into the provided object.
 type Decoder func(io.Reader, any) error
--- pkg/expressgo/encoder.go
+++ pkg/expressgo/encoder.go
@@ -4,7 +4,11 @@ import (
 	"net/http"
 )
 
-// Encoder is a function that encodes an object into a writer
+// Encoder is a function type interface that encodes an object into an HTTP response writer.
+// It takes an http.ResponseWriter to write the encoded data to and any value to encode.
+// Returns an error if the encoding process fails.
 type Encoder func(http.ResponseWriter, any) error
 
+// EncoderDecorator is a higher-order function interface that transforms an Encoder.
+// It takes an existing Encoder and returns a new Encoder with enhanced functionality.
 type EncoderDecorator func(Encoder) Encoder
